37f0fa7e31a0a3a2ce41e8cd9f04fcce836f09a9
David Konsumer <konsumer@jetboystudio.com>
2026-04-27 22:07:59 -0700

    intiial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import { Firehose } from '@skyware/firehose'
+import AhoCorasick from 'aho-corasick-node'
+import { readFile } from 'node:fs/promises'
+
+const keywords = (await readFile('wordlist.txt', 'utf8')).split('\n').map(s => s.trim()).filter(s => s)
+
+// this is more complicated than it needs to be, but does faster matches for >50 keywords
+const builder = AhoCorasick.builder()
+keywords.forEach(k => builder.add(k))
+const ac = builder.build()
+
+const firehose = new Firehose()
+firehose.on('commit', commit => {
+	for (const op of commit.ops) {
+		if (op.action === 'create' && op.path.startsWith('app.bsky.feed.post/')) {
+			const text = op.record?.text
+			if (text) {
+				const matches = ac.match(text.toLowerCase())
+				if (matches.length) {
+					const { action , path, record } = op
+					const { langs, createdAt } = record
+					console.log({ author: commit.repo, action, path, langs, createdAt, text, matches })
+				}
+			}
+		}
+	}
+})
+firehose.start()
